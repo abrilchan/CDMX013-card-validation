afd7f63075ef1c3157be211a259423ecf14d964e
abrilchan <abrilcgallegos@gmail.com>
2022-08-08 10:19:39 -0500

    Proyecto Card Validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import validator from './validator.js';
 
-console.log(validator);
+// console.log(validator);
+
+
+let submitButton = document.querySelector(".submit-button");
+
+let cardNumb = document.querySelector("#cardNumb");
+
+
+cardNumb.addEventListener('input', function(){
+  cardNumb.value=cardNumb.value.replace(/[a-zA-Z]/g,'').replace(' ','');
+})
+
+
+submitButton.addEventListener('click', function(){
+    let creditCardNumber = cardNumb.value;
+
+   validator.isValid(creditCardNumber) ;
+
+    
+
+    validator.maskify(creditCardNumber);
+    
+
+
+  if (creditCardNumber=="")
+  { {document.querySelector(".message").textContent = "Por favor, ingresa un número de tarjeta"}}
+  else if (validator.isValid(creditCardNumber))
+  {document.querySelector(".message").textContent = "¡Tarjeta validada exitosamente!"}
+  else
+  {document.querySelector(".message").textContent = "¡Lo sentimos, tu tarjeta no ha sido validada!"}
+   
+
+  cardNumb.value=validator.maskify(creditCardNumber);
+
+} );
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,63 @@
 const validator = {
   // ...
+  isValid:function(numeros){
+    let cardNumbArray = [];
+    let sum = 0;
+    for (var i=0; i<numeros.length; i++){
+      cardNumbArray.push(parseInt(numeros.split('')[i]));
+  }
+
+  let revCardNum = cardNumbArray.reverse();
+
+  function multiplyNum(numbers) {
+      return numbers.map(function(number, index) {
+        return index % 2 ? number * 2 : number;
+      });
+    }
+
+  let newRevCardNum = multiplyNum(revCardNum);
+
+  let beforeSumCardNum = newRevCardNum.join('');
+  let toSum = [];
+
+  for (var j=0; j<beforeSumCardNum.length; j++){
+    toSum.push(parseInt(beforeSumCardNum.split('')[j]));
+}
+
+  for (let i = 0; i < toSum.length; i++) {
+      sum += toSum[i];
+  }
+
+  let revCardNumMod = sum%10
+
+  if (revCardNumMod === 0)
+    {return true}
+  else
+    {return false;}
+  
+  },
+
+
+maskify:function(card){
+
+  function cardHide(card) {
+    let hideNum = [];
+    for(let i = 0; i < card.length; i++){
+      if(i < card.length-4){
+        hideNum.push("#")
+      }else{
+        hideNum.push(card[i])
+      }
+    }
+    return hideNum.join("");
+
+  }
+
+  return cardHide(card);
+
+}
+
+
 };
 
 export default validator;
